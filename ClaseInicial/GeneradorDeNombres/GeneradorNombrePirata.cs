977e3f4488e3e339be8cd0f49b842c8a61bbef6f
Acadii <142053779+acadii@users.noreply.github.com>
2023-09-07 14:55:59 -0600

    GeneradorNombres

---
+++ ClaseInicial/GeneradorDeNombres/GeneradorNombrePirata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneradorDeNombres
+{
+    class GeneradorNombrePirata
+    {
+        string[] inicialNombre = {"A", "B", "C", "D", "E"};
+        string[] inicialApellido = {"A", "B", "C", "D", "E",};
+        string[] adjetivo = { "Golden", "Black", "Heavenly", "Big", "Flying" };
+        string[] titulo = { "Lion", "Knight", "Sword", "Nose", "Beard" };
+
+        void generar()
+        {
+            
+
+        }
+
+
+    }
+}
